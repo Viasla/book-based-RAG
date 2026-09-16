2ccd27c209bf8309ed69be63d96b98f37a33fa8e
Viacheslav Slaboshpytskyi <v.slaboshpytskyi@tippingpoint.works>
2026-09-16 16:32:55 +0200

    refactor(pdf-03): chagne to list comprehentions

--- scr/pageselector.py
+++ scr/pageselector.py
@@ -5,13 +5,13 @@ from pypdf import PdfReader, PdfWriter
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
-samples = list(map(int, sys.argv[3:]))
+samples = [int(page) - 1 for page in sys.argv[3:]]
 
 input_pdf = PdfReader(input_file)
 
 output_pdf = PdfWriter()
 
 for page in samples:
-    output_pdf.add_page(input_pdf.pages[page - 1])
+    output_pdf.add_page(input_pdf.pages[page])
 
 output_pdf.write(output_file)
