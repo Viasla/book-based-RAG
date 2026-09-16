0b87bcde779c4b95659629afbb1b262977cc1696
Viacheslav Slaboshpytskyi <v.slaboshpytskyi@tippingpoint.works>
2026-09-16 16:39:17 +0200

    feat(pdf-03): Add to page selector script entry point

--- scr/pageselector.py
+++ scr/pageselector.py
@@ -2,16 +2,19 @@ import sys
 
 from pypdf import PdfReader, PdfWriter
 
-input_file = sys.argv[1]
-output_file = sys.argv[2]
 
-samples = [int(page) - 1 for page in sys.argv[3:]]
+def select_pages(input_file, output_file, samples):
+    input_pdf = PdfReader(input_file)
+    output_pdf = PdfWriter()
 
-input_pdf = PdfReader(input_file)
+    for page in samples:
+        output_pdf.add_page(input_pdf.pages[page])
+    
+    output_pdf.write(output_file)
 
-output_pdf = PdfWriter()
+if __name__ == "__main__":
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    samples = [int(page) - 1 for page in sys.argv[3:]]
 
-for page in samples:
-    output_pdf.add_page(input_pdf.pages[page])
-
-output_pdf.write(output_file)
+    select_pages(input_file, output_file, samples)
