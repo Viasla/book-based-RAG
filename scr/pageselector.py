ae4e22eca4d43b52504a4115517f32fe34b4941a
Viacheslav Slaboshpytskyi <v.slaboshpytskyi@tippingpoint.works>
2026-09-16 16:46:29 +0200

    refactor(pdf-03): change function which appends pages
    According to pypdf package documentation, it's recommended to use
    append() instead of add_page()

--- scr/pageselector.py
+++ scr/pageselector.py
@@ -6,10 +6,9 @@ from pypdf import PdfReader, PdfWriter
 def select_pages(input_file, output_file, samples):
     input_pdf = PdfReader(input_file)
     output_pdf = PdfWriter()
-
-    for page in samples:
-        output_pdf.add_page(input_pdf.pages[page])
     
+    output_pdf.append(input_pdf, samples)
+
     output_pdf.write(output_file)
 
 if __name__ == "__main__":
