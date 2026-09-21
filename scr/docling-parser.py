06c31f9f3bca84878625ed5c4a647d99c01e095b
Viacheslav Slaboshpytskyi <v.slaboshpytskyi@tippingpoint.works>
2026-09-21 15:48:46 +0200

    feat(pdf-03): add parsing option for docling script

--- scr/docling-parser.py
+++ scr/docling-parser.py
@@ -1,15 +1,46 @@
-import sys
+import argparse
 
-from docling.document_converter import DocumentConverter
+from docling.datamodel.base_models import InputFormat
+from docling.datamodel.pipeline_options import PdfPipelineOptions
+from docling.document_converter import DocumentConverter, PdfFormatOption
 
 
-def convert(source, destination):
+def convert_default(source, destination):
     converter = DocumentConverter()
     result = converter.convert(source)
     result.document.save_as_markdown(destination)
 
-if __name__ == "__main__":
-    input_file = sys.argv[1]
-    output_file = sys.argv[2]
+def convert_options(source, destination):
+    pipeline_options = PdfPipelineOptions()
 
-    convert(input_file, output_file)
+    pipeline_options.do_formula_enrichment = True
+    pipeline_options.do_table_structure = True
+    pipeline_options.do_ocr = False
+    pipeline_options.force_backend_text = True
+
+    converter = DocumentConverter(
+        format_options={
+            InputFormat.PDF: PdfFormatOption(
+                pipeline_options=pipeline_options
+            )
+        }
+    )
+
+    result = converter.convert(source)
+    result.document.save_as_markdown(destination)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", type=str, help="path to input file")
+    parser.add_argument("output", type=str, help="path to output file")
+    parser.add_argument("--method", type=str, default="default", help="")
+
+    args = parser.parse_args()
+
+    if args.method == "default":
+        convert_default(args.input, args.output)
+    elif args.method == "options":
+        convert_options(args.input, args.output)
+    else:
+        print(f"Unknown method: {args.method}")
